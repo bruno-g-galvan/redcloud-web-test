f5dbb65c5dccc8558086ba3d56befe6be26bc24b
bruno-gab-galvan <bruno.gab.galvan@gmail.com>
2024-12-22 11:21:51 -0300

    first_commit

---
+++ pre-signed-s3/pages/index.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { ChangeEvent } from "react";
+import "./Upload.css";  // Import the CSS file
+
+async function uploadToS3(e: ChangeEvent<HTMLFormElement>) {
+  const formData = new FormData(e.target);
+  const file = formData.get("file");
+
+  if (!file) {
+    return null;
+  }
+
+  // @ts-ignore
+  const fileType = encodeURIComponent(file.type);
+
+  const { data } = await axios.get(`/api/media?fileType=${fileType}`);
+  const { uploadUrl, key } = data;
+
+  await axios.put(uploadUrl, file);
+
+  return key;
+}
+
+function Upload() {
+  async function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const key = await uploadToS3(e);
+  }
+
+  return (
+    <div className="upload-container">
+      <p className="upload-heading">Please select a file to upload</p>
+      <form onSubmit={handleSubmit} className="upload-form">
+        <input
+          type="file"
+          accept="image/jpeg,image/png"
+          name="file"
+          className="upload-file-input"
+        />
+        <button type="submit" className="upload-button">
+          Upload
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Upload;
